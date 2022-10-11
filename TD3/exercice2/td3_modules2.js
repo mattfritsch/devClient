fd1705d4d68747932d39c882421f702d135dc59a
mattfritsch <mattfritsch98380@gmail.com>
2022-10-11 09:25:00 +0200

    TD3 : TD3 final

--- TD3/exercice2/td3_modules2.js
+++ TD3/exercice2/td3_modules2.js
@@ -44,15 +44,21 @@ function saveElements(){
     }
     searchValue = parseInt(document.getElementById("search").value.trim())
 
-    let sumOfElements = td3modules.getSum41(numberArr)
-    let numberOfEven = td3modules.getNumberOfEven42(numberArr)
+    let sumOfElements = td3modules.getSum41(numberArr).toString()
+    let numberOfEven = td3modules.getNumberOfEven42(numberArr).toString()
     let maxEven = td3modules.getMaxEven45(numberArr)
-    let elementPosition = td3modules.dichotomie(numberArr, searchValue)
+    if(numberArr.indexOf(maxEven) === -1){
+        maxEven = "Il n'y a pas de nombre pairs"
+    }
+    else{
+        maxEven = maxEven.toString()
+    }
+    let elementPosition = td3modules.dichotomie(numberArr, searchValue).toString()
 
-    document.getElementById("sum").innerText = sumOfElements.toString()
-    document.getElementById("numberOf").innerText = numberOfEven.toString()
-    document.getElementById("max").innerText = maxEven.toString()
-    document.getElementById("position").innerText = elementPosition.toString()
+    document.getElementById("sum").innerText = sumOfElements
+    document.getElementById("numberOf").innerText = numberOfEven
+    document.getElementById("max").innerText = maxEven
+    document.getElementById("position").innerText = elementPosition
 
 }
 
